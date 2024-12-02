6c1159b6b41c4ee67b7130215d0609afacfb3a29
IsaqueCruzDev <isaquedeoliveiracruz@outlook.com>
2024-12-02 08:46:57 -0300

    remove /

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://timeclockapi-f2hmeaczcjauakfm.ukwest-01.azurewebsites.net/",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://timeclockapi-f2hmeaczcjauakfm.ukwest-01.azurewebsites.net",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json"
